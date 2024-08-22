4020f4dde6fc027e41b0b681865f909a11a4b06d
117503445 <t117503445@gmail.com>
2024-08-22 05:41:04 +0000

    feat: Update dependencies and add proxy authentication to handleHTTPSConnection function
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,31 @@
 module github.com/117503445/https-proxy
 
 go 1.23.0
+
+require (
+	github.com/117503445/goutils v0.0.0-20240817175023-c208c6d669ae
+	github.com/alecthomas/kong v0.9.0
+	github.com/alecthomas/kong-toml v0.2.0
+	github.com/rs/zerolog v1.33.0
+)
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.1.0 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/knadh/koanf/parsers/toml/v2 v2.1.0 // indirect
+	github.com/knadh/koanf/providers/env v0.1.0 // indirect
+	github.com/knadh/koanf/providers/file v1.1.0 // indirect
+	github.com/knadh/koanf/providers/posflag v0.1.0 // indirect
+	github.com/knadh/koanf/providers/structs v0.1.0 // indirect
+	github.com/knadh/koanf/v2 v2.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,15 +5,20 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"strings"
 	"sync"
+
+	"github.com/117503445/goutils"
+	"github.com/rs/zerolog/log"
+
+	"github.com/alecthomas/kong"
+	kongtoml "github.com/alecthomas/kong-toml"
 )
 
-const (
-	username = "your_username"
-	password = "your_password"
+var (
+	username = ""
+	password = ""
 )
 
 func handleHTTPSConnection(clientConn net.Conn) {
@@ -23,7 +28,7 @@ func handleHTTPSConnection(clientConn net.Conn) {
 	buffer := make([]byte, 4096)
 	n, err := clientConn.Read(buffer)
 	if err != nil {
-		log.Println("Error reading CONNECT request:", err)
+		log.Warn().Err(err).Msg("Error reading CONNECT request")
 		return
 	}
 
@@ -31,7 +36,7 @@ func handleHTTPSConnection(clientConn net.Conn) {
 	request := string(buffer[:n])
 	lines := strings.Split(request, "\r\n")
 	if len(lines) < 1 {
-		log.Println("Invalid request")
+		log.Warn().Msg("Invalid request")
 		return
 	}
 
@@ -39,36 +44,38 @@ func handleHTTPSConnection(clientConn net.Conn) {
 	var host string
 	_, err = fmt.Sscanf(requestLine, "CONNECT %s HTTP/1.1", &host)
 	if err != nil {
-		log.Println("Error parsing CONNECT request:", err)
+		log.Warn().Err(err).Str("requestLine", requestLine).Msg("Error parsing request line")
 		return
 	}
 
-	// 验证 Proxy-Authorization 头部
-	authHeader := ""
-	for _, line := range lines {
-		if strings.HasPrefix(line, "Proxy-Authorization:") {
-			authHeader = strings.TrimSpace(line[len("Proxy-Authorization:"):])
-			break
+	if username != "" {
+		// 验证 Proxy-Authorization 头部
+		authHeader := ""
+		for _, line := range lines {
+			if strings.HasPrefix(line, "Proxy-Authorization:") {
+				authHeader = strings.TrimSpace(line[len("Proxy-Authorization:"):])
+				break
+			}
 		}
-	}
 
-	if !isValidAuth(authHeader) {
-		clientConn.Write([]byte("HTTP/1.1 407 Proxy Authentication Required\r\nProxy-Authenticate: Basic realm=\"Access to the proxy\"\r\n\r\n"))
-		log.Println("Invalid Proxy-Authorization header")
-		return
+		if !isValidAuth(authHeader) {
+			clientConn.Write([]byte("HTTP/1.1 407 Proxy Authentication Required\r\nProxy-Authenticate: Basic realm=\"Access to the proxy\"\r\n\r\n"))
+			log.Warn().Msg("Invalid Proxy-Authorization header")
+			return
+		}
 	}
 
 	// 向客户端发送 200 OK 响应
 	_, err = clientConn.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
 	if err != nil {
-		log.Println("Error writing 200 OK response:", err)
+		log.Warn().Err(err).Msg("Error writing 200 OK response")
 		return
 	}
 
 	// 连接到目标服务器
 	serverConn, err := net.Dial("tcp", host)
 	if err != nil {
-		log.Println("Error connecting to target server:", err)
+		log.Warn().Err(err).Str("host", host).Msg("Error connecting to target server")
 		return
 	}
 	defer serverConn.Close()
@@ -106,34 +113,54 @@ func isValidAuth(authHeader string) bool {
 	return credentials == expectedCredentials
 }
 
+var cli struct {
+	Config []string `short:"c" help:"Config files." type:"path" default:"config.toml" env:"CONFIG"`
+
+	Cert     string `help:"Path to the certificate." type:"path" required:"true" default:"server.crt" env:"CERT"`
+	Key      string `help:"Path to the private key." type:"path" required:"true" default:"server.key" env:"KEY"`
+	Port     int    `help:"Port to listen on." default:"443" env:"PORT"`
+	Username string `help:"Username for proxy authentication." env:"USERNAME"`
+	Password string `help:"Password for proxy authentication." env:"PASSWORD"`
+}
+
 func main() {
-	// 创建一个自签名的 TLS 证书
-	cert, err := tls.LoadX509KeyPair("server.crt", "server.key")
-	if err != nil {
-		log.Fatal("Error loading X509 key pair:", err)
+	goutils.InitZeroLog()
+
+	kong.Parse(&cli)
+	kong.Parse(&cli, kong.Configuration(kongtoml.Loader, cli.Config...))
+
+	log.Info().Str("cert", cli.Cert).Str("key", cli.Key).Int("port", cli.Port).Str("username", cli.Username).Strs("config", cli.Config).Msg("Load Config")
+
+	if cli.Username == "" && cli.Password == "" {
+		log.Info().Msg("No username and password provided, disabling authentication")
+	} else if cli.Username != "" && cli.Password != "" {
+		log.Info().Msg("Using provided username and password for authentication")
+	} else {
+		log.Fatal().Msg("Both username and password must be provided")
 	}
 
-	// 配置 TLS
+	cert, err := tls.LoadX509KeyPair(cli.Cert, cli.Key)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error loading X509 key pair")
+	}
+
 	tlsConfig := &tls.Config{Certificates: []tls.Certificate{cert}}
 
-	// 创建一个监听器
-	listener, err := tls.Listen("tcp", ":20001", tlsConfig)
+	listener, err := tls.Listen("tcp", fmt.Sprintf(":%d", cli.Port), tlsConfig)
 	if err != nil {
-		log.Fatal("Error creating listener:", err)
+		log.Fatal().Err(err).Msg("Error creating listener")
 	}
 	defer listener.Close()
 
-	log.Println("HTTPS proxy server is running on port 20001")
+	log.Info().Int("port", cli.Port).Msg("HTTPS proxy server is running")
 
 	for {
-		// 接受客户端连接
 		clientConn, err := listener.Accept()
 		if err != nil {
-			log.Println("Error accepting connection:", err)
+			log.Warn().Err(err).Msg("Error accepting connection")
 			continue
 		}
 
-		// 处理客户端连接
 		go handleHTTPSConnection(clientConn)
 	}
 }
